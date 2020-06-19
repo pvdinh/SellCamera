f143604084d390ddbb3d6ca8b4720d4499da6839
bodinh <61085282+bodinh@users.noreply.github.com>
2020-06-19 14:41:35 +0700

    Update History Order

--- SellCamera/Controllers/HomeController.cs
+++ SellCamera/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SellCamera.Models;
+using SellCamera.Models.OtherClass;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +11,8 @@ namespace SellCamera.Controllers
 {
     public class HomeController : Controller
     {
+        SellCameraDataModel db = new SellCameraDataModel();
+        Cart cart = new Cart();
         public ActionResult Index()
         {
             Session["user"] = 1003;
@@ -26,6 +31,25 @@ namespace SellCamera.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult HistoryOrder()
+        {
+            if(Session["user"] != null)
+            {
+                int MaKH = int.Parse(Session["user"].ToString());
+                //lấy lịch sử order
+                List<HistoryOrder> list = db.Database.SqlQuery<HistoryOrder>("history_order @idKH", new SqlParameter("@idKH", MaKH)).OrderByDescending(s=>s.MaDH).ToList();
+                ViewBag.countinhistory = list.Count();
+                ViewBag.data = list;
+                //lấy lịch sử sản phẩm đã order
+                cart.listInCart = db.Database.SqlQuery<Cart>("Load_Cart_History @idKH", new SqlParameter("@idKH", MaKH)).ToList();
+                return View(cart.listInCart);
+            }
+            else
+            {
+                ViewBag.countinhistory = 0;
+                return View();
+            }
         }
     }
 }
---
+++ SellCamera/Models/OtherClass/HistoryOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SellCamera.Models.OtherClass
+{
+    public class HistoryOrder
+    {
+        public int MaDH { get; set; }
+        public DateTime Ngaydatmua { get; set; }
+        public string ghichu { get; set; }
+    }
+}
