9c278e7d0bab7a916d9bf03360e8da3995594447
NghiemLam <thunderbrucelee@gmail.com>
2020-06-23 18:39:56 +0700

    Add NhaCungCapController

---
+++ SellCamera/Areas/ADMIN/Controllers/NhaCungCapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SellCamera.Areas.ADMIN.Controllers
+{
+    public class NhaCungCapController : Controller
+    {
+        // GET: ADMIN/NhaCungCap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
