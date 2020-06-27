70319d0d1c10d5b6c18d893d7405a6664742d515
catchcodercat9907 <hungqtmta2021@gmail.com>
2020-06-27 22:41:55 +0700

    ProductController and its Index View

---
+++ SellCamera/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using SellCamera.Models;
+using SellCamera.Models.OtherClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace SellCamera.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        SellCameraDataModel db = new SellCameraDataModel();
+        public ActionResult Index(int? id)
+        {
+            Session["id"] = id;
+
+            var spkm = db.SPkhuyenmais.Where(s => s.MaSP == id).FirstOrDefault();
+            if (spkm != null)
+            {
+                var ttkm = db.Khuyenmais.Where(s => s.MaKM == spkm.MaSPKM).FirstOrDefault();
+                if (ttkm != null)
+                    ViewBag.ttkm = ttkm;
+            }
+
+            //kiểm tra hàng còn hay không
+            InfoProduct data = new InfoProduct();
+            List<InfoProduct> model = new List<InfoProduct>();
+            if (data.MaSP == id)
+            {
+                model = db.Database.SqlQuery<InfoProduct>("get_product_discount").ToList();
+            }
+            var status = db.Sanphams.Where(s => s.MaSP == id).Select(s => s.SoLuong).FirstOrDefault();
+            ViewBag.status = status;
+            return View(model);
+        }
+    }
+}
