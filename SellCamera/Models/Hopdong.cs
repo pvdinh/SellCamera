824500a8122ea575436fc6acda0279232064b339
NghiemLam <thunderbrucelee@gmail.com>
2020-06-29 01:00:09 +0700

    Update-NCC&Add-HopDongPage

--- SellCamera/Models/Hopdong.cs
+++ SellCamera/Models/Hopdong.cs
@@ -1,7 +1,8 @@
-namespace SellCamera.Models
+﻿namespace SellCamera.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -11,17 +12,26 @@ namespace SellCamera.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DisplayName("Mã Hợp Đồng")]
         public int IDHopdong { get; set; }
-
+        [Required]
+        [DisplayName("Mã Sản Phẩm")]
         public int IDSP { get; set; }
-
+        [Required]
+        [DisplayName("Mã NCC")]
         public int IDNhacungcap { get; set; }
-
+        [Required]
+        [DisplayName("Ngày Ký")]
         public DateTime? Ngayky { get; set; }
-
+        [Required]
+        [DisplayName("Số Lượng")]
         public int? soluong { get; set; }
-
+        [Required]
+        [DisplayName("Tổng Tiền")]
         public double? tongtien { get; set; }
+
+
+        
 
         public virtual Nhacungcap Nhacungcap { get; set; }
 
