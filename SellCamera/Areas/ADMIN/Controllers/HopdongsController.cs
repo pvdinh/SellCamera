21cb2c77957f1d4a027db151ced0e4bb365765de
catchcodercat9907 <hungqtmta2021@gmail.com>
2020-06-30 14:00:44 +0700

    commit

---
+++ SellCamera/Areas/ADMIN/Controllers/HopdongsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SellCamera.Models;
+
+namespace SellCamera.Areas.ADMIN.Controllers
+{
+    public class HopdongsController : Controller
+    {
+        private SellCameraDataModel db = new SellCameraDataModel();
+
+        
+        public ActionResult Index()
+        {
+            var hopdongs = db.Hopdongs.Include(h => h.Nhacungcap).Include(h => h.Sanpham);
+            return View(hopdongs.ToList());
+        }
+
+       
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hopdong hopdong = db.Hopdongs.Find(id);
+            if (hopdong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hopdong);
+        }
+
+       
+        public ActionResult Create()
+        {
+            ViewBag.IDNhacungcap = new SelectList(db.Nhacungcaps, "IDNhacungcap", "TenNhaungcap");
+            ViewBag.IDSP = new SelectList(db.Sanphams, "MaSP", "TenSP");
+            return View();
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDHopdong,IDSP,IDNhacungcap,Ngayky,soluong,tongtien")] Hopdong hopdong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Hopdongs.Add(hopdong);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDNhacungcap = new SelectList(db.Nhacungcaps, "IDNhacungcap", "TenNhaungcap", hopdong.IDNhacungcap);
+            ViewBag.IDSP = new SelectList(db.Sanphams, "MaSP", "TenSP", hopdong.IDSP);
+            return View(hopdong);
+        }
+
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hopdong hopdong = db.Hopdongs.Find(id);
+            if (hopdong == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDNhacungcap = new SelectList(db.Nhacungcaps, "IDNhacungcap", "TenNhaungcap", hopdong.IDNhacungcap);
+            ViewBag.IDSP = new SelectList(db.Sanphams, "MaSP", "TenSP", hopdong.IDSP);
+            return View(hopdong);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDHopdong,IDSP,IDNhacungcap,Ngayky,soluong,tongtien")] Hopdong hopdong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hopdong).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDNhacungcap = new SelectList(db.Nhacungcaps, "IDNhacungcap", "TenNhaungcap", hopdong.IDNhacungcap);
+            ViewBag.IDSP = new SelectList(db.Sanphams, "MaSP", "TenSP", hopdong.IDSP);
+            return View(hopdong);
+        }
+
+        
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Hopdong hopdong = db.Hopdongs.Find(id);
+            if (hopdong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hopdong);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Hopdong hopdong = db.Hopdongs.Find(id);
+            db.Hopdongs.Remove(hopdong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SellCamera/Areas/ADMIN/Controllers/NhacungcapsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SellCamera.Areas.ADMIN.model;
+using SellCamera.Models;
+
+namespace SellCamera.Areas.ADMIN.Controllers
+{
+    public class NhacungcapsController : Controller
+    {
+        private SellCameraDataModel db = new SellCameraDataModel();
+
+      
+        public ActionResult Index()
+        {
+            
+            return View(db.Nhacungcaps.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nhacungcap nhacungcap = db.Nhacungcaps.Find(id);
+            if (nhacungcap == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhacungcap);
+        }
+
+     
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+     
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDNhacungcap,TenNhaungcap,phonenumber,email")] Nhacungcap nhacungcap)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                db.Nhacungcaps.Add(nhacungcap);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nhacungcap);
+        }
+
+      
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nhacungcap nhacungcap = db.Nhacungcaps.Find(id);
+            if (nhacungcap == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhacungcap);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDNhacungcap,TenNhaungcap,phonenumber,email")] Nhacungcap nhacungcap)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.Entry(nhacungcap).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nhacungcap);
+        }
+
+       
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nhacungcap nhacungcap = db.Nhacungcaps.Find(id);
+            if (nhacungcap == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhacungcap);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Nhacungcap nhacungcap = db.Nhacungcaps.Find(id);
+            db.Nhacungcaps.Remove(nhacungcap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
